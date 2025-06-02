dff5bcc27b3e3980d2b1dea2a08272826532f415
Eetu Soronen <eetu.soronen@tuni.fi>
2025-06-02 11:55:10 +0300

    remove const from inputs_count

--- include/openasip.h
+++ include/openasip.h
@@ -12,13 +12,13 @@ extern unpackInstructionFn *openasip_unpackInstruction;
 
 typedef int executeInstruction32Fn(const char *opName,
                                    const uint32_t *inputs,
-                                   const uint32_t inputs_count,
+                                   uint32_t inputs_count,
                                    uint32_t *output,
                                    char **error_msg);
 extern executeInstruction32Fn *openasip_executeInstruction32;
 typedef int executeInstruction64Fn(const char *opName,
                                    const uint64_t *inputs,
-                                   const uint64_t inputs_count,
+                                   uint64_t inputs_count,
                                    uint64_t *output,
                                    char **error_msg);
 extern executeInstruction64Fn *openasip_executeInstruction64;
